d1be9fd2f7f61ae3cd1ba8909bb9124d51ead146
pennypang <huiminpang720@gmail.com>
2023-01-07 16:55:36 -0800

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function kgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+//# sourceMappingURL=index.js.map
